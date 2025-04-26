3115ed529fd0392d53f177037f0207da539161eb
ZAKEYU MATIAS <BSC/MAT/01/19@COMLAB.COM>
2025-04-26 23:18:23 +0300

    trying to connect to firebase

--- AllFunctions_copy_20250426225811_copy_20250426230113.ino
+++ AllFunctions_copy_20250426225811_copy_20250426230113.ino
@@ -1,37 +1,31 @@
-/*
-  FILE: AllFunctions.ino
-  // AUTHOR: Koby Hale
-  PURPOSE: Test functionality
-*/
-
 #define TINY_GSM_MODEM_SIM7000
-#define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
+#define TINY_GSM_RX_BUFFER 1024
 #define SerialAT Serial1
 
-// See all AT commands, if wanted
 #define DUMP_AT_COMMANDS
-
-/*
-   Tests enabled
-*/
 #define TINY_GSM_TEST_GPRS    true
-#define TINY_GSM_TEST_GPS     true
+#define TINY_GSM_TEST_GPS     false
 #define TINY_GSM_POWERDOWN    true
-
-// set GSM PIN, if any
-#define GSM_PIN ""
-
-// Your GPRS credentials, if any
-const char apn[]  = "internet.tnm.mw";     //SET TO YOUR APN
-const char gprsUser[] = "tnm";
-const char gprsPass[] = "tnm";
 
 #include <TinyGsmClient.h>
 #include <SPI.h>
 #include <SD.h>
 #include <Ticker.h>
+#include <ArduinoHttpClient.h>
 
-#ifdef DUMP_AT_COMMANDS  // if enabled it requires the streamDebugger lib
+#define GSM_PIN ""  // Leave empty if SIM card doesn't require a PIN
+
+const char apn[] = "internet.tnm.mw";  // TNM Malawi APN
+const char gprsUser[] = "tnm";
+const char gprsPass[] = "tnm";
+
+// Firebase settings
+const char* FIREBASE_HOST = "faults-2a6ef-default-rtdb.firebaseio.com";
+const char* FIREBASE_AUTH = "1RUKKBU2nlDeFOJMsAd4HZudtGoGC8aragbRIsNg";
+const char* FIREBASE_IP = "34.120.160.131";  // Firebase IP
+const int FIREBASE_PORT = 443;
+
+#ifdef DUMP_AT_COMMANDS
 #include <StreamDebugger.h>
 StreamDebugger debugger(SerialAT, Serial);
 TinyGsm modem(debugger);
@@ -39,8 +33,8 @@ TinyGsm modem(debugger);
 TinyGsm modem(SerialAT);
 #endif
 
-#define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
-#define TIME_TO_SLEEP  60          // Time ESP32 will go to sleep (in seconds)
+#define uS_TO_S_FACTOR 1000000ULL
+#define TIME_TO_SLEEP  60  // Sleep time in seconds
 
 #define UART_BAUD   115200
 #define PIN_DTR     25
@@ -54,182 +48,65 @@ TinyGsm modem(SerialAT);
 #define SD_CS       13
 #define LED_PIN     12
 
-
-int counter, lastIndex, numberOfPieces = 24;
-String pieces[24], input;
-
-
 void setup()
 {
-    // Set console baud rate
     Serial.begin(115200);
     delay(10);
 
-    // Set LED OFF
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, HIGH);
 
     pinMode(PWR_PIN, OUTPUT);
     digitalWrite(PWR_PIN, HIGH);
-    // Starting the machine requires at least 1 second of low level, and with a level conversion, the levels are opposite
     delay(1000);
     digitalWrite(PWR_PIN, LOW);
 
+    // Initialize SD Card (optional)
     SPI.begin(SD_SCLK, SD_MISO, SD_MOSI, SD_CS);
     if (!SD.begin(SD_CS)) {
         Serial.println("SDCard MOUNT FAIL");
     } else {
         uint32_t cardSize = SD.cardSize() / (1024 * 1024);
-        String str = "SDCard Size: " + String(cardSize) + "MB";
-        Serial.println(str);
+        Serial.println("SDCard Size: " + String(cardSize) + "MB");
     }
-
-    Serial.println("\nWait...");
 
     delay(1000);
-
     SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
 
-    // Restart takes quite some time
-    // To skip it, call init() instead of restart()
     Serial.println("Initializing modem...");
     if (!modem.restart()) {
-        Serial.println("Failed to restart modem, attempting to continue without restarting");
+        Serial.println("Failed to restart modem");
     }
-
-
-
-
-
 }
 
 void loop()
 {
-
-    // Restart takes quite some time
-    // To skip it, call init() instead of restart()
     Serial.println("Initializing modem...");
     if (!modem.init()) {
-        Serial.println("Failed to restart modem, attempting to continue without restarting");
+        Serial.println("Failed to init modem");
     }
 
-    String name = modem.getModemName();
-    delay(500);
-    Serial.println("Modem Name: " + name);
-
-    String modemInfo = modem.getModemInfo();
-    delay(500);
-    Serial.println("Modem Info: " + modemInfo);
-
-    // Set Modem GPS Power Control Pin to LOW ,turn off GPS power
-    // Only in version 20200415 is there a function to control GPS power
     modem.sendAT("+CGPIO=0,48,1,0");
-    if (modem.waitResponse(10000L) != 1) {
-        DBG("Set GPS Power LOW Failed");
-    }
+    modem.waitResponse(10000L);
 
 #if TINY_GSM_TEST_GPRS
-    // Unlock your SIM card with a PIN if needed
-    if ( GSM_PIN && modem.getSimStatus() != 3 ) {
+    if (GSM_PIN && modem.getSimStatus() != 3) {
         modem.simUnlock(GSM_PIN);
     }
-#endif
 
     modem.sendAT("+CFUN=0");
-    if (modem.waitResponse(10000L) != 1) {
-        DBG(" +CFUN=0  false ");
-    }
+    modem.waitResponse(10000L);
     delay(200);
 
-    /*
-      2 Automatic
-      13 GSM only
-      38 LTE only
-      51 GSM and LTE only
-    * * * */
-
-    bool res = modem.setNetworkMode(2);
-    if (!res) {
-        DBG("setNetworkMode  false ");
-        return ;
-    }
+    modem.setNetworkMode(2);  // Auto
     delay(200);
-
-    /*
-      1 CAT-M
-      2 NB-Iot
-      3 CAT-M and NB-IoT
-    * * */
-    res = modem.setPreferredMode(3);
-    if (!res) {
-        DBG("setPreferredMode  false ");
-        return ;
-    }
+    modem.setPreferredMode(3);  // LTE only
     delay(200);
-
-    /*AT+CBANDCFG=<mode>,<band>[,<band>…]
-     * <mode> "CAT-M"   "NB-IOT"
-     * <band>  The value of <band> must is in the band list of getting from  AT+CBANDCFG=?
-     * For example, my SIM card carrier "NB-iot" supports B8.  I will configure +CBANDCFG= "Nb-iot ",8
-     */
-    /* modem.sendAT("+CBANDCFG=\"NB-IOT\",8 ");
-     if (modem.waitResponse(10000L) != 1) {
-         DBG(" +CBANDCFG=\"NB-IOT\" ");
-     }
-     delay(200);*/
-
     modem.sendAT("+CFUN=1");
-    if (modem.waitResponse(10000L) != 1) {
-        DBG(" +CFUN=1  false ");
-    }
+    modem.waitResponse(10000L);
     delay(200);
 
-#if TINY_GSM_TEST_GPRS
-
-    SerialAT.println("AT+CGDCONT?");
-    delay(500);
-    if (SerialAT.available()) {
-        input = SerialAT.readString();
-        for (int i = 0; i < input.length(); i++) {
-            if (input.substring(i, i + 1) == "\n") {
-                pieces[counter] = input.substring(lastIndex, i);
-                lastIndex = i + 1;
-                counter++;
-            }
-            if (i == input.length() - 1) {
-                pieces[counter] = input.substring(lastIndex, i);
-            }
-        }
-        // Reset for reuse
-        input = "";
-        counter = 0;
-        lastIndex = 0;
-
-        for ( int y = 0; y < numberOfPieces; y++) {
-            for ( int x = 0; x < pieces[y].length(); x++) {
-                char c = pieces[y][x];  //gets one byte from buffer
-                if (c == ',') {
-                    if (input.indexOf(": ") >= 0) {
-                        String data = input.substring((input.indexOf(": ") + 1));
-                        if ( data.toInt() > 0 && data.toInt() < 25) {
-                            modem.sendAT("+CGDCONT=" + String(data.toInt()) + ",\"IP\",\"" + String(apn) + "\",\"0.0.0.0\",0,0,0,0");
-                        }
-                        input = "";
-                        break;
-                    }
-                    // Reset for reuse
-                    input = "";
-                } else {
-                    input += c;
-                }
-            }
-        }
-    } else {
-        Serial.println("Failed to get PDP!");
-    }
-
-
-    Serial.println("\n\n\nWaiting for network...");
+    Serial.println("Waiting for network...");
     if (!modem.waitForNetwork()) {
         delay(10000);
         return;
@@ -239,96 +116,55 @@ void loop()
         Serial.println("Network connected");
     }
 
-    Serial.println("\n---Starting GPRS TEST---\n");
-    Serial.println("Connecting to: " + String(apn));
+    Serial.println("Connecting to GPRS...");
     if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+        Serial.println("GPRS connect failed");
         delay(10000);
         return;
     }
 
-    Serial.print("GPRS status: ");
     if (modem.isGprsConnected()) {
-        Serial.println("connected");
+        Serial.println("GPRS connected");
+
+        // Simulated electricity status (replace with sensor logic)
+        bool electricityPresent = random(0, 2);
+        String status = electricityPresent ? "present" : "absent";
+        Serial.println("Electricity status: " + status);
+
+        // Send to Firebase
+        TinyGsmClient client(modem);
+        HttpClient http(client, FIREBASE_IP, FIREBASE_PORT);  // IP-based for SIM7000
+
+        String path = "/electricityStatus.json?auth=" + String(FIREBASE_AUTH);
+        String jsonPayload = "{\"electricity\":\"" + status + "\"}";
+
+        http.beginRequest();
+        http.put(path);  // Use PUT to write directly
+        http.sendHeader("Host", FIREBASE_HOST);
+        http.sendHeader("Content-Type", "application/json");
+        http.sendHeader("Content-Length", jsonPayload.length());
+        http.beginBody();
+        http.print(jsonPayload);
+        http.endRequest();
+
+        int statusCode = http.responseStatusCode();
+        String response = http.responseBody();
+
+        Serial.println("Firebase Status: " + String(statusCode));
+        Serial.println("Response: " + response);
     } else {
-        Serial.println("not connected");
+        Serial.println("GPRS not connected");
     }
 
-    String ccid = modem.getSimCCID();
-    Serial.println("CCID: " + ccid);
-
-    String imei = modem.getIMEI();
-    Serial.println("IMEI: " + imei);
-
-    String cop = modem.getOperator();
-    Serial.println("Operator: " + cop);
-
-    IPAddress local = modem.localIP();
-    Serial.println("Local IP: " + String(local));
-
-    int csq = modem.getSignalQuality();
-    Serial.println("Signal quality: " + String(csq));
-
-    SerialAT.println("AT+CPSI?");     //Get connection type and band
-    delay(500);
-    if (SerialAT.available()) {
-        String r = SerialAT.readString();
-        Serial.println(r);
-    }
-
-    Serial.println("\n---End of GPRS TEST---\n");
-#endif
-
-#if TINY_GSM_TEST_GPRS
     modem.gprsDisconnect();
     if (!modem.isGprsConnected()) {
         Serial.println("GPRS disconnected");
-    } else {
-        Serial.println("GPRS disconnect: Failed.");
     }
 #endif
-
-#if TINY_GSM_TEST_GPS
-    Serial.println("\n---Starting GPS TEST---\n");
-    // Set Modem GPS Power Control Pin to HIGH ,turn on GPS power
-    // Only in version 20200415 is there a function to control GPS power
-    modem.sendAT("+CGPIO=0,48,1,1");
-    if (modem.waitResponse(10000L) != 1) {
-        DBG("Set GPS Power HIGH Failed");
-    }
-
-    modem.enableGPS();
-
-    float lat,  lon;
-    while (1) {
-        if (modem.getGPS(&lat, &lon)) {
-            Serial.printf("lat:%f lon:%f\n", lat, lon);
-            break;
-        } else {
-            Serial.print("getGPS ");
-            Serial.println(millis());
-        }
-        delay(2000);
-    }
-    modem.disableGPS();
-
-    // Set Modem GPS Power Control Pin to LOW ,turn off GPS power
-    // Only in version 20200415 is there a function to control GPS power
-    modem.sendAT("+CGPIO=0,48,1,0");
-    if (modem.waitResponse(10000L) != 1) {
-        DBG("Set GPS Power LOW Failed");
-    }
-
-    Serial.println("\n---End of GPRS TEST---\n");
-#endif
-
 
 #if TINY_GSM_POWERDOWN
-    // Try to power-off (modem may decide to restart automatically)
-    // To turn off modem completely, please use Reset/Enable pins
     modem.sendAT("+CPOWD=1");
-    if (modem.waitResponse(10000L) != 1) {
-        DBG("+CPOWD=1");
-    }
+    modem.waitResponse(10000L);
     modem.poweroff();
     Serial.println("Poweroff.");
 #endif
@@ -336,6 +172,4 @@ void loop()
     esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
     delay(200);
     esp_deep_sleep_start();
-
-    Serial.println("This will never be printed");
 }
